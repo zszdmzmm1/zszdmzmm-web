7703c0329899f57f66ce9ecec276d7ed882d9064
zszdmzmm <1075534380@qq.com>
2023-08-07 23:56:52 +0800

    插件 给哔哩哔哩视频合集加上预计剩余时间 基本功能完善完毕

--- day0807/bilibili-plugin/scripts/content.js
+++ day0807/bilibili-plugin/scripts/content.js
@@ -1,5 +1,3 @@
-let isFirst = true;
-
 setInterval(function () {
     let timeElement = document.querySelectorAll("div.video-episode-card .video-episode-card__info-duration");
     let finalTime = 0;
@@ -10,36 +8,47 @@ setInterval(function () {
                 isStart = true;
             }
             if (isStart) {
-                let timeGroup = element.innerText.split(":");
-                let singleTime = 0;
-                for (let i = 0; i < timeGroup.length; i++) {
-                    singleTime = singleTime * 60 + parseInt(timeGroup[i]);
-                }
+                let singleTime = getSeconds(element.innerText);
                 finalTime += singleTime;
             }
         }
     })
+    let currentVideoSeconds = 0;
+    let currentVideoSecondsElement = document.querySelector("span.bpx-player-ctrl-time-current");
+    if (currentVideoSecondsElement) {
+        currentVideoSeconds = getSeconds(currentVideoSecondsElement.innerText);
+        finalTime -= currentVideoSeconds;
+    }
     let hour, minute, second;
     hour = Math.floor(finalTime / 3600);
     minute = Math.floor(finalTime / 60) % 60;
     second = finalTime % 60;
-    if(minute < 10){
-        minute = "0" + minute;
-    }
-    if(second < 10){
-        second = "0" + second;
-    }
+    minute = timePattenParse(minute);
+    second = timePattenParse(second);
     let finalTimeString = hour + ":" + minute + ":" + second;
     let element = document.createElement("div");
     element.innerText = "剩余时长:" + finalTimeString;
     element.className = "remainTime";
     element.style = "margin-right: 8px";
-    if(!document.querySelector("div.remainTime")){
-        //isFirst = false;
+    if (!document.querySelector("div.remainTime")) {
         document.querySelector(".play-num").after(element);
-    }else{
+    } else {
         document.querySelector("div.remainTime").innerText = element.innerText;
     }
 }, 1000);
 
+function getSeconds(timeString) {
+    let timeArr = timeString.split(":");
+    let timeSecond = 0;
+    for (let i = 0; i < timeArr.length; i++) {
+        timeSecond = timeSecond * 60 + parseInt(timeArr[i]);
+    }
+    return timeSecond;
+}
 
+function timePattenParse(timeNum) {
+    if (timeNum < 10) {
+        return "0" + timeNum;
+    }
+    return timeNum;
+}
